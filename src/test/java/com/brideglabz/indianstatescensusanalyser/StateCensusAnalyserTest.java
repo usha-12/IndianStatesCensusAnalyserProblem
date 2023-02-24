fef4f26689cf23ad24021a26b385fa46b5e65add
usha-12 <usha12ahirwar@gmail.com>
2023-02-24 13:44:35 +0530

    Added: Given the StateCode File when correct but csv header

--- src/test/java/com/brideglabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/brideglabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
@@ -112,4 +112,16 @@ public class StateCensusAnalyserTest {
             Assertions.assertEquals("Oops, it seems the files weren't separated by comma check the delimeter", message);
         }
     }
+    @Test
+    void readStateCodeTestingHEADER_MISMATCHException() throws IOException, CsvException, CustomException {
+        try {
+            File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\HeaderMismatchStateCode.csv");
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            stateCensusAnalyser.readeStateCSV(actualFile);
+        }
+        catch (CustomException e) {
+            String message = e.getMessage();
+            Assertions.assertEquals("It seems the header is not match, please check the header", message);
+        }
+    }
 }
