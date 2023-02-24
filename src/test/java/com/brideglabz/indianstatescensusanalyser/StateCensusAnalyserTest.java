8bd28dbbb3af75cb438d9e21928350288d3a35e1
usha-12 <usha12ahirwar@gmail.com>
2023-02-24 13:05:21 +0530

    Added: Given the StateCode File if incorrect Returns a custom Exce…

--- src/test/java/com/brideglabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/brideglabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class StateCensusAnalyserTest {
     List<CSVStates> statesActualList;
     List<CSVStateCensus> actualList;
+
     @Test
     void readCSVCheckingRecordMatches() throws IOException, CsvException, CustomException {
         File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\StateCensusData.csv");
@@ -17,54 +18,55 @@ public class StateCensusAnalyserTest {
         int expected = 35;
         Assertions.assertEquals(actual, expected);
     }
+
     @Test
     void readCSVTestingFILE_Not_FOUNDException() throws IOException, CsvException, CustomException {
         try {
             File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\StateCensusData.csv");
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
             stateCensusAnalyser.readCSV(actualFile);
-        }
-        catch (CustomException e) {
+        } catch (CustomException e) {
             String message = e.getMessage();
             Assertions.assertEquals("Oops!, it seems the file doesn't exist", message);
         }
     }
+
     @Test
     void readCSVTestingFILE_TYPE_MISMATCHException() throws IOException, CsvException, CustomException {
         try {
             File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\StateCensusData.pdf");
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
             stateCensusAnalyser.readCSV(actualFile);
-        }
-        catch (CustomException e) {
+        } catch (CustomException e) {
             String message = e.getMessage();
             Assertions.assertEquals("Oops!, it seems the file type doesn't match", message);
         }
     }
+
     @Test
     void readCSVTestingDELIMETER_INCORRECTException() throws IOException, CsvException, CustomException {
         try {
             File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\DelimeterIncorrectData.csv");
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
             stateCensusAnalyser.readCSV(actualFile);
-        }
-        catch (CustomException e) {
+        } catch (CustomException e) {
             String message = e.getMessage();
             Assertions.assertEquals("Oops, it seems the files weren't separated by comman check the delimeter", message);
         }
     }
+
     @Test
     void readCSVTestingHEADER_MISMATCHException() throws IOException, CsvException, CustomException {
         try {
             File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\HeaderMismatch.csv");
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
             stateCensusAnalyser.readCSV(actualFile);
-        }
-        catch (CustomException e) {
+        } catch (CustomException e) {
             String message = e.getMessage();
             Assertions.assertEquals("It seems the header is not match, please check the header", message);
         }
     }
+
     @Test
     void readStateCodeCheckingRecordMatches() throws IOException, CsvException, CustomException {
         File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\StateCode.csv");
@@ -73,5 +75,17 @@ public class StateCensusAnalyserTest {
         int actual = statesActualList.size();
         int expected = 35;
         Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void readStateCodeTestingFILE_Not_FOUNDException() throws IOException, CsvException, CustomException {
+        try {
+            File actualFile = new File("C:\\Users\\User\\Desktop\\243-rfp\\IndianStatesCensusAnalyser\\src\\main\\resources\\StateCod.csv");
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            stateCensusAnalyser.readeStateCSV(actualFile);
+        } catch (CustomException e) {
+            String message = e.getMessage();
+            Assertions.assertEquals("Oops!, it seems the file doesn't exist", message);
+        }
     }
 }
